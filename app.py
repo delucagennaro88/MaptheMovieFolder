f907d8940c558c2bbe1782d55ff6aabe64e8a92c
delucagennaro88 <delucagennaro88@gmail.com>
2018-08-21 16:47:11 +0200

    Work in progress 4

--- app.py
+++ app.py
@@ -3,8 +3,26 @@ from flask import Flask, flash, url_for, redirect, render_template
 from json_manager import search_director, search_cast, open_json
 import sys, imdb
 
+actor_dictionary = {}
+movie_dictionary = {}
+actor_collection = {}
 
 ia = imdb.IMDb()
+
+def search_filmography(filmography):
+
+    for a in filmography:
+        for key, value in a.items():
+            alfa = 0
+            key_index = key #qui va salvato l'indice della categoria
+            actor_dictionary[key_index] = []
+            for b in value:
+                movie_film = ia.search_movie(str(b))
+                movie = movie_film[0]
+                title = movie.get('title')
+                movie_year = movie['year']
+                actor_dictionary[key_index].append({'Title': title, 'Year': movie_year})
+    return actor_dictionary
 
 def attori_amati(actor_name):
 #1. Dato il nome di un attore, restituisce l'elenco di tutti i suoi film (solo titoli e date)
@@ -22,17 +40,14 @@ def attori_amati(actor_name):
 
         filmography = actor_identifier['filmography']
 
-        for a in filmography:
-            for key, value in a.items():
-                print(key.upper()) #qui va salvato l'indice della categoria
-                for b in value:
-                    movie_film = ia.search_movie(str(b))
-                    movie = movie_film[0]
-                    title = movie.get('title')
-                    movie_year = movie['year']
-                    print(title + '\n' + str(movie_year))
-                    #movie_id = movie.movieID
-                    #movie_identifier = ia.get_movie(movie_id)
+        filmography_box = search_filmography(filmography)
+
+        actor_collection[actor] = []
+        actor_collection[actor].append({'Nome': actor, 'Date': dates, 'Filmography': filmography_box})
+
+        #qui si verifica se il JSON esiste
+        #se non esiste si crea e si scrive con W
+        #se esiste si aggiorna col metodo inventato
 
     except imdb.IMDbError as e:
         print("Probably you're not connected to Internet.  Complete error report:")
